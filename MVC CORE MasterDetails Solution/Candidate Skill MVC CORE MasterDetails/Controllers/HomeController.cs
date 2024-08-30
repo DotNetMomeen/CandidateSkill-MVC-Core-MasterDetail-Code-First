1b0f5a58769b869c8615e7b1f236d59b2bb1f17c
DotNetMomeen <dotnetmomeen@gmail.com>
2024-08-31 05:45:35 +0600

    CandidateSkill-MVC-Core-MasterDetail-Code-First

---
+++ MVC CORE MasterDetails Solution/Candidate Skill MVC CORE MasterDetails/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Candidate_Skill_MVC_CORE_MasterDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC CORE MasterDetails Solution/Candidate Skill MVC CORE MasterDetails/Models/CandidateDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Candidate_Skill_MVC_CORE_MasterDetails.Models
+{
+    public class CandidateDbContext:DbContext
+    {
+        public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<CandidateSkill> CandidateSkills { get; set; }
+    }
+}
